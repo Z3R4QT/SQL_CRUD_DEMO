bd79764daf2da67222c2e37fa6927cc9a9fa62f4
Z3R4QT <christopher092480@gmail.com>
2025-11-21 19:13:29 +0800

    added insert data

--- SQL_CRUD_DEMO/Form1.Designer.vb
+++ SQL_CRUD_DEMO/Form1.Designer.vb
@@ -23,28 +23,120 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnConnect1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.btnCreate = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnConnect1
         '
-        Me.btnConnect1.Location = New System.Drawing.Point(39, 93)
+        Me.btnConnect1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnConnect1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConnect1.Location = New System.Drawing.Point(89, 64)
         Me.btnConnect1.Name = "btnConnect1"
-        Me.btnConnect1.Size = New System.Drawing.Size(225, 154)
+        Me.btnConnect1.Size = New System.Drawing.Size(259, 154)
         Me.btnConnect1.TabIndex = 0
         Me.btnConnect1.Text = "Connect My SQL"
-        Me.btnConnect1.UseVisualStyleBackColor = True
+        Me.btnConnect1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(89, 241)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 29)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(89, 276)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 32)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "age"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(89, 317)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 28)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Email"
+        '
+        'txtName
+        '
+        Me.txtName.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.txtName.Location = New System.Drawing.Point(186, 238)
+        Me.txtName.Multiline = True
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(162, 32)
+        Me.txtName.TabIndex = 4
+        '
+        'txtAge
+        '
+        Me.txtAge.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.txtAge.Location = New System.Drawing.Point(186, 273)
+        Me.txtAge.Multiline = True
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(162, 35)
+        Me.txtAge.TabIndex = 5
+        '
+        'txtEmail
+        '
+        Me.txtEmail.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.txtEmail.Location = New System.Drawing.Point(186, 314)
+        Me.txtEmail.Multiline = True
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(162, 31)
+        Me.txtEmail.TabIndex = 6
+        '
+        'btnCreate
+        '
+        Me.btnCreate.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnCreate.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCreate.Location = New System.Drawing.Point(89, 370)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(259, 68)
+        Me.btnCreate.TabIndex = 7
+        Me.btnCreate.Text = "Insert Data"
+        Me.btnCreate.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(565, 538)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnConnect1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnConnect1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents btnCreate As Button
 End Class
--- SQL_CRUD_DEMO/Form1.vb
+++ SQL_CRUD_DEMO/Form1.vb
@@ -5,8 +5,6 @@ Public Class Form1
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
 
-
-
     Private Sub btnConnect1_Click(sender As Object, e As EventArgs) Handles btnConnect1.Click
 
         conn = New MySqlConnection
@@ -18,6 +16,28 @@ Public Class Form1
         Catch ex As MySqlException
             MessageBox.Show("Connection Failed: " & ex.Message)
             conn.Close()
+        End Try
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+
+        Dim query As String = "INSERT INTO crud_demo_db.`students_tbl` (Name, age, email) VALUES ( @name, @age, @email);"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@name", txtName.Text)
+                    cmd.Parameters.AddWithValue("@age", CInt(txtAge.Text))
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record Insert Successful!")
+                    txtName.Clear()
+                    txtAge.Clear()
+                    txtEmail.Clear()
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
         End Try
     End Sub
 End Class
